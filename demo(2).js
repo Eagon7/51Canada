6aa69ac1dabd7a32eb511a70ff697f1d5b1fbb5b
gyhoshi <gjs120511>
2022-10-12 20:16:38 +0800

    no message

--- demo(2).js
+++ demo(2).js
@@ -1,3 +1,17 @@
+// https://juejin.cn/post/6916087983808626701
+//         reduce 接收 2 个参数：
+// 第一个参数是回调函数（必选），第二个参数是初始值 initialValue（可选） 。
+// 而第一个参数（回调函数），接收下面四个参数：
+
+// Accumulator (acc) (累计器)
+// Current Value (cur) (当前值)
+// Current Index (idx) (当前索引)
+// Source Array (src) (源数组)
+
+//initialValue ⚠️ 初始值 initialValue 可以是任意类型。如果没有提供 initialValue，reduce 会从索引 1 的地方开始执行 callback 方法，跳过第一个索引。如果提供 initialValue，从索引 0 开始。
+//上句话解读: [1,2,3,4] 如果给initialValue,则从1开始执行---> initialValue+1 ....  .
+//            如果没有给初始值,则从2开始,上一个值acc为1, 1+2
+
 // product为购物车商品数据
 let product = [
     {
